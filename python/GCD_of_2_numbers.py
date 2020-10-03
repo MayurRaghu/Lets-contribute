706404c23f11945eba9a41ff93098bdde1b13196
MayurRaghu <67970719+MayurRaghu@users.noreply.github.com>
2020-10-03 10:48:17 +0530

    Create GCD_of_2_numbers.py

---
+++ python/GCD_of_2_numbers.py
@@ -0,0 +1,10 @@
+# Initialize the 2 numbers
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+i = 1
+while(i <= num1 and i <= num2):
+  if(num1 % i == 0 and num2 % i == 0):
+    gcd = i
+  i = i + 1
+print("GCD is", gcd)
+© 2020 GitHub, Inc.
